3962691bc6ce051ef90641cf645850ef34b6276a
AriesWuu <103198780+AriesWuu@users.noreply.github.com>
2022-04-08 10:22:41 +0800

    Update Local_queue.cpp

--- Local_queue.cpp
+++ Local_queue.cpp
@@ -12,23 +12,47 @@ Local_queue::Local_queue() {
     return;
 }
 
-void Local_queue::deal(int date) //process and record the status, date, and time for all the patients in queue.
+void Local_queue::deal(int date) //record all the patients, and be ready to pass them to central queue
 {
-    Person_Node temp = p->front();
-    for(int i = 0; i<p->size(); i++){
-        if(temp.regday = date){
-            q->push(temp);
+    for(int i = 0; i<p->size(); i++){  //iterate the input queue
+        Person_Node *A = new Person_Node;  //give a new space for this node
+        *A = p->front();
+        if(A->get_regday() == date){  //if that node/person's register day is this date
+            p->pop();
+            Person_Node *B;   
+            B=A;  
+            q->push(*B);      //push this node to q queue to give to central
+            Person_Node *C;
+            C = A;
+            total->push(*C);  //store this node to database
         }
-        total.push(temp);
-    }
-    for(int i = 0; i < total->size(); i++){
-        if(temp.lastday = date){
-            q->pop();
-            e->push(temp);
+        else{
+            p->pop();         //if not, give it back to p queue
+            Person_Node *B;
+            B=A;  
+            p->push(*B);
         }
-        total.push(temp);
+        
     }
-
+    for(int i = 0; i<total->size(); i++){  //iterate the total queue
+        Person_Node *A = new Person_Node;
+        *A = p->front();
+        if(A->lastday == date){  //if that node/person is at the ddl at this date
+            p->pop();
+            Person_Node *B;
+            B=A;  
+            e->push(*B);      //push this node to e queue to give to central
+            Person_Node *C;
+            C = A;
+            total->push(*C);  //store this node to database
+        }
+        else{
+            total->pop();         //if not at ddl, give it back to total queue
+            Person_Node *B;
+            B=A;
+            total->push(*B);  
+        }
+    }
 }
 
 queue<Person_Node>* Local_queue::get_p(void)
@@ -42,7 +66,16 @@ queue<Person_Node>* Local_queue::get_q(void)
     return q;
 }
 
-// search the exact person recursively, according to the key and ID; if not find, return NULL
+queue<Person_Node>* Local_queue::get_e(void)
+{
+    return e;
+}
+
+queue<Person_Node>* Local_queue::get_total(void)
+{
+    return total;
+}
+
 void Local_queue::search_id(int id){
     Person_Node a[p->size()];
     for (int i = 0; i < p->size(); i++)
@@ -56,4 +89,6 @@ void Local_queue::search_id(int id){
         }
     }
 }
+
+
 
